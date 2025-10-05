2893fca561d143268ebdfd84704096e0235736a6
william courtney <timcourtney@mac.com>
2025-10-05 14:07:37 -0400

    Add scraper implementation and comprehensive tests

---
+++ bfskinner_scrape/__init__.py
@@ -0,0 +1,5 @@
+"""Package providing tools to scrape B.F. Skinner related content."""
+
+from .scraper import ArticleRecord, Scraper, ScraperError
+
+__all__ = ["ArticleRecord", "Scraper", "ScraperError"]
